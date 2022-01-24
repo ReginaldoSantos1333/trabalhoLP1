610423d30c8b9214d2de84ae1129ce838fde2837
ReginaldoSantos1333 <60099179+ReginaldoSantos1333@users.noreply.github.com>
2022-01-23 23:57:22 -0300

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,236 +1,109 @@
-import ssl
+from distutils import command
 from tkinter import *
-from time import *
-
-"""
-Cria um botão:
-----botao = Button(janela, text="Iniciar", command= tarefas)
-
-Adiciona a posição de um botão:
-----botao.grid(column=0, row=1)
-
-Adiciona um label:
-----texto_teste = Label(janela, text="")
-
-Indica posicao de um label:
-----texto_teste.grid(column=0, row=2)
-
-"""
+from tkinter import messagebox
+from tkinter import Tk
 
 
-def tarefas():
-    janela2 = Tk()
-    janela2.title("Tarefas")
-    # Texto informando o nome da janela
-    texto_tarefas = Label(janela2, text="Tarefas") 
-    texto_tarefas.grid(column=1, row=0)
-
-    # Botão que segue para alteração das tarefas difíceis
-    botaoTD = Button(janela2, text="Tarefas Difíceis", command=tarefasDificeis)
-    botaoTD.grid(column=0, row=1, padx=10, pady= 10)
-
-    # Botão que segue para alteração das tarefas Faceis
-    botaoTF = Button(janela2, text="Tarefas Faceis", command=tarefasFaceis)
-    botaoTF.grid(column=1, row=1, padx=10, pady= 10)
-
-    # Botão que segue para alteração das tarefas Fixas
-    botaoTFi = Button(janela2, text="Tarefas Fixas", command=tarefasFixas)
-    botaoTFi.grid(column=2, row=1, padx=10, pady= 10)
-    janela2.mainloop()
-
-def tarefasDificeis():
-        janela4 = Tk()
-        #titulo da janela
-        janela4.title("Tarefas Dificeis")
-
-        #Texto na janela
-        texto_tarefasD = Label(janela4, text="Lista de Tarefas Dificeis")
-        texto_tarefasD.grid(column=1, row=0)
+def mensagem(tipomsg,aba):
+            messagebox.showinfo(title ="UPday", message = "Bom te conhecer {}".format(tipomsg))
+            aba.destroy()
         
-        #Botao de remover
-        botaoErase = Button(janela4, text="Remover", command= remover)
-        botaoErase.grid(column=0, row= 1, padx=10, pady=10)
-
-        #Botao de editar
-        botaoEdit = Button(janela4, text="Editar", command= edit)
-        botaoEdit.grid(column=0, row = 2, padx=10, pady=10)
-
-        #Botao de adicionar
-        botaoAdd = Button(janela4, text="Adicionar", command= add)
-        botaoAdd.grid(column=2, row= 1, padx=10, pady=10)
 
 
-        #Botao de salvar
-        botaoSave = Button(janela4, text="Salvar", command= save)
-        botaoSave.grid(column=2, row= 2, padx=10, pady=10)
-
-        janela4.mainloop()
-
-def tarefasFaceis():
-        janela5 = Tk()
-        janela5.title("Tarefas Fáceis")
-
-        texto_tarefasF = Label(janela5, text="Lista de Tarefas Fáceis")
-        texto_tarefasF.grid(column=1, row=0)
-
-        #Botao de remover
-        botaoErase = Button(janela5, text="Remover", command= remover)
-        botaoErase.grid(column=0, row= 1, padx=10, pady=10)
-
-        #Botao de editar
-        botaoEdit = Button(janela5, text="Editar", command= edit)
-        botaoEdit.grid(column=0, row = 2, padx=10, pady=10)
+#inserçãorotina
 
 
-        #Botao de adicionar
-        botaoAdd = Button(janela5, text="Adicionar", command= add)
-        botaoAdd.grid(column=2, row= 1, padx=10, pady=10)
-
-
-        #Botao de salvar
-        botaoSave = Button(janela5, text="Salvar", command= save)
-        botaoSave.grid(column=2, row= 2, padx=10, pady=10)
-
-        janela5.mainloop()
-
-def tarefasFixas():
-        janela6 = Tk()
-        janela6.title("Tarefas Fixas")
-
-        texto_tarefasFix = Label(janela6, text="Lista de Tarefas Fixas")
-        texto_tarefasFix.grid(column=1, row=0)
-        
-        #Botao de remover
-        botaoErase = Button(janela6, text="Remover", command= remover)
-        botaoErase.grid(column=0, row= 1, padx=10, pady=10)
-
-
-        #Botao de editar
-        botaoEdit = Button(janela6, text="Editar", command= edit)
-        botaoEdit.grid(column=0, row = 2, padx=10, pady=10)
-
-
-        #Botao de adicionar
-        botaoAdd = Button(janela6, text="Adicionar", command= add)
-        botaoAdd.grid(column=2, row= 1, padx=10, pady=10)
-
-
-        #Botao de salvar
-        botaoSave = Button(janela6, text="Salvar", command= save)
-        botaoSave.grid(column=2, row= 2, padx=10, pady=10)
-
-        janela6.mainloop()
-
-
-def save():
+def inserir():
     pass
-def edit():
-    pass
-def remover():
-    pass
-def add():
+
+    #editar rotina
+
+def editar():
     pass
 
 
-def novarotina():
-    janela3 = Tk()
-    janela3.title("Nova_Rotina")
-    # Texto informando o nome da janela
-    texto_novarotina = Label(janela3, text="Selecione o tipo de rotina à ser criada:")
-    texto_novarotina.grid(column=1, row=0)
+    #buscar rotina
+ 
+def buscar():
+    pass
 
-    # Botão que cria um escopo de rotina PADRÃO
-    botaoDefault = Button(janela3, text="Padrão", command=rotinaPadrao)
-    botaoDefault.grid(column=0, row=1, padx=10, pady=10)
 
-    # Botão que cria um escopo de rotina VAZIA
-    botaoVazia = Button(janela3, text="Vazia", command=rotinaVazia)
-    botaoVazia.grid(column=2, row=1, padx=10, pady=10)
-    
+    #visualizar rotina
+
+def ver():
+    pass
+
+
+
+    # upday rotina
    
-    janela3.mainloop()
-
-def rotinaPadrao():
-     #adiciona janela principal 
-    janelaPadrao = Tk()
-
-    #Adiciona um título da janela invocada
-    janelaPadrao.title("Rotina")
-
-    #Informação na janela
-    texto_inicial = Label(janelaPadrao, text="Gerador de Rotina") 
-    texto_inicial.grid(column=2, row=0, padx=100, pady=200)
-
-    #chama a tela de tarefa
-    botao = Button(janelaPadrao, text="Tarefas", command= tarefas)
-    botao.grid(column=3, row=3,padx= 10, pady = 10)
-
-    #chama a tela de nova rotina
-    botao = Button(janelaPadrao, text="Nova Rotina", command= novarotina)
-    botao.grid(column=0, row=1, padx= 10, pady = 10)
-
-    #chama a tela de organizar
-    botao = Button(janelaPadrao, text="Organizar", command= organizar)
-    botao.grid(column=0, row=3,padx= 10, pady = 10)
-
-    # roda janela
-    janelaPadrao.mainloop()
-
-def rotinaVazia():
-    #adiciona janela principal 
-    janelaVazia = Tk()
-
-    #Adiciona um título da janela invocada
-    janelaVazia.title("Rotina")
-
-    #Informação na janela
-    texto_inicial = Label(janelaVazia, text="Gerador de Rotina") 
-    texto_inicial.grid(column=2, row=0, padx=100, pady=200)
-
-    #chama a tela de tarefa
-    botao = Button(janelaVazia, text="Tarefas", command= tarefas)
-    botao.grid(column=3, row=3,padx= 10, pady = 10)
-
-    #chama a tela de nova rotina
-    botao = Button(janelaVazia, text="Nova Rotina", command= novarotina)
-    botao.grid(column=0, row=1, padx= 10, pady = 10)
-
-    #chama a tela de organizar
-    botao = Button(janelaVazia, text="Organizar", command= organizar)
-    botao.grid(column=0, row=3,padx= 10, pady = 10)
-
-    # roda janela
-    janelaVazia.mainloop()
-
-
-def organizar():
+def upday():
     pass
 
 
 
-#adiciona janela principal 
-janela = Tk()
-
-#Adiciona um título da janela invocada
-janela.title("Rotina")
-
-#Informação na janela
-texto_inicial = Label(janela, text="Gerador de Rotina") 
-texto_inicial.grid(column=2, row=0, padx=100, pady=200)
+#apagar rotina
+def apagar():
+    pass
 
 
-#chama a tela de tarefa
-botao = Button(janela, text="Tarefas", command= tarefas)
-botao.grid(column=3, row=3,padx= 10, pady = 10)
+    #encerrar sessão
 
-#chama a tela de nova rotina
-botao = Button(janela, text="Nova Rotina", command= novarotina)
-botao.grid(column=0, row=1, padx= 10, pady = 10)
 
-#chama a tela de organizar
-botao = Button(janela, text="Organizar", command= organizar)
-botao.grid(column=0, row=3,padx= 10, pady = 10)
+#Boas-vindas
 
-# roda janela
+welcome = Tk()
+vnum_cstexto = StringVar()
+# Mensagem de boas vindas
+welcome.title("Bem vindo ao UPday!")
+welcomelbl = Label(welcome, text= """Olá! Bem vindo(a) à UPday!
+Juntos podemos dar um 'Up' no seu dia ;)
+Primeiramente, seria ótimo te conhecer mais um pouco.
+        Vamos começar?!""")
+welcomelbl.grid(column=0,row=0)
+# Caixa de inserção
+welcometab = Entry(welcome, textvariable=vnum_cstexto)
+welcometab.grid(column=0, row= 3)
+# Mensagem na caixa de texto
+vnum_cstexto.set("Nos diga o seu nome!")
+# Botão de inserção
+welcomebtn = Button(welcome, text="Mostrar mensagem", command=lambda:mensagem(vnum_cstexto.get(),welcome))
+welcomebtn.grid(column =0, row = 4)
+welcome.mainloop()
 
-janela.mainloop()
+# Janela principal
+
+mainwindow = Tk()
+# Muda a corda janela principal
+mainwindow.configure(background="lightblue")
+# Titulo da janela
+mainwindow.title("UPday")
+# Tamanho janela principal
+#mainwindow.geometry("1280x720")
+# Traz janela para frente 
+mainwindow.lift()
+#Label com o nome do app, cor de fundo,texto e sua fonte
+texto_inicial = Label(mainwindow, text = "UPday",bg = "lightblue",fg = "black")
+texto_inicial.configure(font=("Segoe Print", 25))
+texto_inicial.grid(column=1,row=0, padx = 100,pady = 300)
+
+# Botão de inserção
+insert = Button(mainwindow, text = "Nova Rotina", command= inserir)
+insert.grid(column=0,row=6, padx = 10,pady = 10)
+# Botão de edição
+edit = Button(mainwindow, text= "Editar Rotina", command= editar)
+edit.grid(column=0,row=7, padx = 10,pady = 10)
+# Botão de buscar
+search = Button(mainwindow, text= "Buscar Rotina", command=buscar)
+search.grid(column=0,row=8, padx = 10,pady = 10)
+# Botão de visualiza
+check = Button(mainwindow, text= "Ver Rotina", command=ver)
+check.grid(column=2,row=6, padx = 10,pady = 10)
+# Botão de upday
+Upd = Button(mainwindow, text = "UPday Rotina", command=upday)
+Upd.grid(column=2,row=7, padx = 10,pady = 10)
+# Botão de apagar
+erase = Button(mainwindow, text = "Apagar Rotina", command=apagar)
+erase.grid(column=2,row=8, padx = 10,pady = 10)
+
+mainwindow.mainloop()
