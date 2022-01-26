2a9e7ef3738e8dba96302f924c8cbf28c7cf69e1
ReginaldoSantos1333 <60099179+ReginaldoSantos1333@users.noreply.github.com>
2022-01-26 02:09:18 -0300

    Create bemvindo.py

---
+++ bemvindo.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from principal import *
+
+
+janela1 = Tk()
+
+text_label = Label(janela1, text = """Olá! Bem vindo(a) à UPday!")
+   Juntos podemos dar um 'Up' no seu dia ;)")
+    Primeiramente, seria ótimo te conhecer mais um pouco. Vamos começar?!
+    =====================================================
+""")
+
+janela1.mainloop()
+
+
